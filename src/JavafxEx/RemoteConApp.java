fddff51762feee29d25e7bcf07ff2adb31ac5d2b
gisleste <gisleste@gmail.com>
2018-03-19 11:39:47 +0100

    code

--- src/JavafxEx/RemoteConApp.java
+++ src/JavafxEx/RemoteConApp.java
@@ -1,5 +1,23 @@
 package JavafxEx;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class RemoteConApp {
 
-}
+public class RemoteConApp extends Application{
+	 @Override
+	    public void start(Stage primaryStage) throws Exception{
+	        Parent root = FXMLLoader.load(getClass().getResource("RemoteController.fxml"));
+	        primaryStage.setTitle("Here you can control the destructoBot");
+	        primaryStage.setScene(new Scene(root, 800, 500));
+	        primaryStage.show();
+	    }
+
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+	}
+
